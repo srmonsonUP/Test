a7a14c285348c7731f702b0e6695b9ebb49339bb
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-05-26 15:14:31 -0500

    file upload to plot works

--- server.R
+++ server.R
@@ -1,7 +1,18 @@
+options(error = browser)
+
 shinyServer(function(input,output){
   
+  output$plot = renderPlot({
+    plot(nplot())
+  })
   
-  
-  
+  nplot = eventReactive(input$go,
+  {
+    file = input$file
+    if(is.null(file))
+        return(NULL)
+    
+    read.csv(file$datapath, header = T)
+  })
   
 })
--- ui.R
+++ ui.R
@@ -4,12 +4,24 @@ shinyUI(fluidPage(
     
     sidebarPanel(
                  
-      h1("Basic information about the setting that will be here")             
-                 
-                 
+      h1("Basic information about the setting that will be here"),          
+      fileInput("file", "Enter file to load",
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
+      ),
+      actionButton("go", "Plot")
+      
     ),
     
-    mainPanel()
-    
+    mainPanel(
+      plotOutput("plot")
     )
+      
+  )
 ))
